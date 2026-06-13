c2aa5bbd3b314dac11a48f9a7938e15d91262970
Emma-puncher <113306036@g.nccu.edu.tw>
2026-06-13 21:15:07 +0800

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,11 +73,18 @@ document.querySelectorAll('.js-add-to-cart')
         matchingItem.quantity += 1;
       } else {
         cart.push({
-        productId: productId,
-        quantity: 1
-      });
+          productId: productId,
+          quantity: 1
+        });
       }
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
       
-      console.log(cart);
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
     });
   });
